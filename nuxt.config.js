af3b761a0d6a2627320b3062cd6361e3dfc40704
Primaël Owald Lebrun <priimost@gmail.com>
2023-05-11 17:06:56 +0000

    dd

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,9 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
+    server: {
+        port: process.env.NODE_ENV ==  3000 // default: 3000
+      }, 
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
